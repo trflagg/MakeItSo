ce754f4fc3844498fa52842d80484380ea9e088b
Taylor Flagg <trflagg@gmail.com>
2014-08-06 16:12:28 -0400

    Test profile db save & load

--- test/testProfile.js
+++ test/testProfile.js
@@ -9,10 +9,10 @@ describe('Profile object', function() {
     var id;
 
     it('should make a new object', function() {
-
         var Profile = require('../models/profile')(db);
-
         profile = db.create('Profile');
+        assert.equal(profile._name, null);
+        assert.equal(profile._handiness, null);
     }),
 
     it('should modify name', function() {
@@ -25,6 +25,27 @@ describe('Profile object', function() {
         profile.handiness = 'right';
         profile.handiness.should.equal('right');
         profile._handiness.should.equal('right');
+    }),
+
+    it('should save to the db', function(done) {
+        db.save('Profile', profile, function(err, saved_profile) {
+            assert.equal(err, null);
+
+            saved_profile.should.not.equal(null);
+            id = saved_profile._id;
+
+            done();
+        })
+    }),
+
+    it('should load from the db', function(done) {
+        db.load('Profile', {id: id}, function(err, loaded_profile) {
+            assert.equal(err, null);
+
+            loaded_profile.name.should.equal('Taylor')
+
+            done();
+        })
     })
 
 
