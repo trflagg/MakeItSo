6c1e4c37c0d07942bc96cd8252784552bdc93b32
Taylor Flagg <trflagg@gmail.com>
2014-08-06 16:12:10 -0400

    Add handiness to profile model

--- models/profile.js
+++ models/profile.js
@@ -11,16 +11,19 @@ module.exports = function(db) {
 
     Profile.prototype.initialize = function() {
         this._name = null;
+        this._handiness = null;
     };
 
     Profile.prototype.saveToDoc = function(doc) {
         doc.name = this._name;
+        doc.handiness = this._handiness;
 
         return doc;
     };
 
     Profile.prototype.loadFromDoc = function(doc) {
         this._name = doc.name;
+        this._handiness = doc.handiness;
     };
 
     Object.defineProperty(Profile.prototype, "name",
@@ -30,6 +33,16 @@ module.exports = function(db) {
         }
         , set: function(name) {
             this._name = name;
+        }
+    });
+
+    Object.defineProperty(Profile.prototype, "handiness",
+    {
+        get: function() {
+            return this._handiness;
+        }
+        , set: function(handiness) {
+            this._handiness = handiness;
         }
     });
 
--- test/testProfile.js
+++ test/testProfile.js
@@ -1,17 +1,31 @@
 var should = require('should')
+    , assert = require('assert')
     , Db = require('argieDB/db')
     , environment = require('argieDB/environment-local')
     , db = new Db(environment);
 
 describe('Profile object', function() {
+    var profile;
+    var id;
+
     it('should make a new object', function() {
 
         var Profile = require('../models/profile')(db);
 
-        var profile = db.create('Profile');
+        profile = db.create('Profile');
+    }),
 
+    it('should modify name', function() {
         profile.name = 'Taylor';
         profile.name.should.equal('Taylor');
         profile._name.should.equal('Taylor');
+    }),
+
+    it('should modify handiness', function() {
+        profile.handiness = 'right';
+        profile.handiness.should.equal('right');
+        profile._handiness.should.equal('right');
     })
+
+
 });
