f938be3352282854b3975c4d50d7159645d5c382
Taylor Flagg <trflagg@gmail.com>
2014-08-10 18:06:06 -0400

    Remove PUT /profile

--- controllers/profileController.js
+++ controllers/profileController.js
@@ -10,36 +10,5 @@ var render = require('../render')
 
 module.exports = function(app, db) {
 
-    /**
-     * PUT /profile
-     *
-     * make a new profile with sent data
-     * @return {json}
-     */
-    app.put('/profile', bodyParser(), requiredParams(['name']), addProfile);
-    function *addProfile() {
-        var body = this.request.body;
-
-        // make new profile
-        var profile = db.create('Profile');
-        profile.name = body.name;
-
-        try {
-            // save
-            yield db.save('Profile', profile)
-
-            // return id
-            this.body = {
-                id: profile._id
-                , success: 'true'
-            }
-        }
-        catch (e) {
-            this.body = {
-                success: 'false'
-                , error: e.message
-            }
-        }
-    }
 }
 
