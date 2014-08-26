59b5e44a6105b34fd23ea84384cbdb71fc23bf4a
Taylor Flagg <trflagg@gmail.com>
2014-08-26 11:40:24 -0400

    Add helpers.loadModels()

--- test/helpers.js
+++ test/helpers.js
@@ -1,4 +1,5 @@
-var Db = require('argieDB/db')
+var fs = require('fs')
+    , Db = require('argieDB/db')
     , environment = require('argieDB/environment-local')
 
 module.exports = function() {
@@ -6,6 +7,12 @@ module.exports = function() {
 
     helpers.getDb = function() {
         return new Db(environment);
+    }
+
+    helpers.loadModels = function(db) {
+        fs.readdirSync('./models').forEach(function(file) {
+            require('../models/' + file)(db);
+        });
     }
 
     return helpers;
