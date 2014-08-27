858bc803beeeba44336178b8d46bdf8f0ccb4ff1
Taylor Flagg <trflagg@gmail.com>
2014-08-27 18:03:32 -0400

    testProfile uses coDb and yield

--- test/testProfile.js
+++ test/testProfile.js
@@ -8,7 +8,7 @@ describe('Profile object', function() {
         , db;
 
     before(function() {
-        db = helpers.getDb();
+        db = helpers.getCoDb();
         require('../models/profile')(db);
     });
 
@@ -38,72 +38,65 @@ describe('Profile object', function() {
         profile.sex.should.equal('female');
     }),
 
-    it('should fail to save if name is too short', function(done) {
+    it('should fail to save if name is too short', function*() {
         profile.name = 'yo';
-        db.save('Profile', profile, function(err, saved_profile) {
-            err.message.should.equal('name must be at least 3 characters.');
-            done();
-        });
+        try {
+            yield db.save('Profile', profile);
+        } catch(e) {
+            e.message.should.equal('name must be at least 3 characters.');
+        }
     });
 
-    it('should fail to save if name is not only letters', function(done) {
+    it('should fail to save if name is not only letters', function*() {
         profile.name = 'yo.p';
-        db.save('Profile', profile, function(err, saved_profile) {
-            err.message.should.equal('name may only contain uppercase and lowercase letters.');
-            done();
-        });
+        try {
+            yield db.save('Profile', profile);
+        } catch(e) {
+            e.message.should.equal('name may only contain uppercase and lowercase letters.');
+        }
     });
 
-    it('should fail to save if handiness is not right or left', function(done) {
+    it('should fail to save if handiness is not right or left', function*() {
         profile.name = 'Taylor';
         profile.handiness = 'something';
-        db.save('Profile', profile, function(err, saved_profile) {
-            err.message.should.equal('handiness must be either right or left.');
-            done();
-        });
+        try {
+            yield db.save('Profile', profile);
+        } catch(e) {
+            e.message.should.equal('handiness must be either right or left.');
+        }
     });
 
-    it('should fail to save if sex is not male or female', function(done) {
+    it('should fail to save if sex is not male or female', function*() {
         profile.name = 'Taylor';
         profile.handiness = 'left';
         profile.sex = 'something';
-        db.save('Profile', profile, function(err, saved_profile) {
+        try {
+            yield db.save('Profile', profile);
+        } catch(err) {
             err.message.should.equal('sex must be either male or female.');
-            done();
-        });
+        }
     });
 
-    it('should save to the db', function(done) {
+    it('should save to the db', function*() {
         profile.name = 'Taylor';
         profile.handiness = 'right';
         profile.sex = 'male';
-        db.save('Profile', profile, function(err, saved_profile) {
-            assert.equal(err, null);
-            saved_profile.should.not.equal(null);
-            id = profile._id;
-            done();
-        })
+        yield db.save('Profile', profile);
+        id = profile._id;
     }),
 
-    it('should load from the db', function(done) {
-        db.load('Profile', {_id: id}, function(err, loaded_profile) {
-            assert.equal(err, null);
-            loaded_profile.name.should.equal('Taylor');
-            loaded_profile.handiness.should.equal('right');
-            loaded_profile.sex.should.equal('male');
-            done();
-        })
+    it('should load from the db', function*() {
+        var loaded_profile = yield db.load('Profile', {_id: id});
+        loaded_profile.name.should.equal('Taylor');
+        loaded_profile.handiness.should.equal('right');
+        loaded_profile.sex.should.equal('male');
     }),
 
-    it('should only load from projection', function(done) {
-        db.load('Profile', {_id: id}, {name: 1}, function(err, loaded_profile) {
-            assert.equal(err, null);
-            loaded_profile.name.should.equal('Taylor');
-            loaded_profile.should.not.have.property('handiness');
-            loaded_profile.should.not.have.property('sex');
-
-            done();
-        })
+    it('should only load from projection', function*() {
+        var loaded_profile = yield db.load('Profile', {_id: id}, {name: 1});
+        loaded_profile.name.should.equal('Taylor');
+        loaded_profile.should.not.have.property('handiness');
+        loaded_profile.should.not.have.property('sex');
     })
 
 
