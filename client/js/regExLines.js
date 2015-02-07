2fc704abc417be87c845d9408510f2ba741822ce
Taylor Flagg <trflagg@gmail.com>
2015-02-06 19:30:00 -0500

    Captains show individual colors in chat

--- client/js/regExLines.js
+++ client/js/regExLines.js
@@ -2,11 +2,34 @@ define([], function() {
 
   regExLines = [
   {
-    regEx: /^(\[Rdml. Collins\])(.*)$/
+    regEx: /^(\[Rdml. Collins\]:)(.*)$/
     , functionBody: function(currentLine, args) {
-      return "~" + currentLine + "~";
+      return "<span class='collins'>" + args[1] + "</span>" + args[2];
     }
-  }];
+  }, {
+    regEx: /^(\[Cpt. MacKenzie\]:)(.*)$/
+    , functionBody: function(currentLine, args) {
+      return "<span class='mackenzie'>" + args[1] + "</span>" + args[2];
+    }
+  }, {
+    regEx: /^(\[Cpt. Hawkins\]:)(.*)$/
+    , functionBody: function(currentLine, args) {
+      return "<span class='hawkins'>" + args[1] + "</span>" + args[2];
+    }
+  }, {
+    regEx: /^(\[Cpt. Golz\]:)(.*)$/
+    , functionBody: function(currentLine, args) {
+      return "<span class='golz'>" + args[1] + "</span>" + args[2];
+    }
+  }, {
+    regEx: /^(\[Cpt. Banerjee\]:)(.*)$/
+    , functionBody: function(currentLine, args) {
+      return "<span class='banerjee'>" + args[1] + "</span>" + args[2];
+    }
+
+  }
+
+  ];
 
   return regExLines;
 });
