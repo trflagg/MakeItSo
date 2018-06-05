ea541d1040e744893a4c979bc6226acf07c9719c
Taylor Flagg <trflagg@gmail.com>
2018-06-05 19:14:19 -0400

    Parse urlencoded requests

--- app.js
+++ app.js
@@ -89,6 +89,7 @@ app.set('view engine', 'ejs');
 app.use(cookieParser(hiddenDBString));
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 
 const compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, {
