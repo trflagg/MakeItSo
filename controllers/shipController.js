6dca2c559b0803c2c8c6869a4f966532ddeb4607
Taylor Flagg <trflagg@gmail.com>
2018-07-16 20:58:36 -0400

    Save handiness into global

--- controllers/shipController.js
+++ controllers/shipController.js
@@ -47,6 +47,7 @@ module.exports = function(app, db) {
       ship.setGlobal('name', req.params.profile.name);
       ship.setGlobal('gender', req.params.profile.sex);
       ship.setGlobal('ship_name', ship.shipName);
+      ship.setGlobal('handiness', req.params.profile.handiness);
       await ship.startGame();
 
       await db.save('Ship', ship);
